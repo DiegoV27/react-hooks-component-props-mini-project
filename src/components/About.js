775d4d77b6763ea0fc8230b96613a14bb34eb244
Diego Valero <diego27jay@gmail.com>
2022-02-03 14:59:25 -0700

    commit tree 827e2abc02630c91ff04abc31ff96065d08cbde9

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+
+function About() {
+    return (
+        <div className="About">
+        <img 
+          src="https://via.placeholder.com/215"
+          alt="blog logo"
+          />
+      <p>About</p>
+    </div>
+    );
+}
+
+export default About 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <div className="Header">
+            <h1>overreacted</h1>
+        </div>
+
+    );
+}
+export default Header 
